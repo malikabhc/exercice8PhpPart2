dbfce2ca404a810be4558bc3aea1ac2f534940f9
Malikabhc <malika.bouhcine@gmail.com>
2018-07-27 11:11:32 +0200

    exercice8-2

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+  $isOk = true ;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 8/2</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        if ($isOk) {
+          echo 'c\'est ok !!';
+        } else {
+          echo 'c\'est pas bon !!!';
+        }
+      ?>
+    </p>
+  </body>
+</html>
